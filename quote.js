d4e0f4c661d993b8707702677a54c1952f8583c9
shaoli-lu <shaoli_lu@yahoo.com>
2022-12-10 19:30:04 -0600

    random

--- quote.js
+++ quote.js
@@ -5,15 +5,22 @@ getQuote();
 
 })
 
+
+function generateNumbers(max) {
+    return Math.floor(Math.random() * (max + 1))
+}
+
+
 function getQuote() {
+    let i = generateNumbers(10);
     fetch("https://quotesondesign.com/wp-json/wp/v2/posts/?content=rand").then(response => response.json()).then(data => {
     //https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand
     //https://quotesondesign.com/wp-json/wp/v2/posts/?name=rand
     //https://quotesondesign.com/wp-json/wp/v2/posts/?title=rand
     //https://quotesondesign.com/wp-json/
         // document.querySelector('#quote').innerHTML = '"' + data[0].content.rendered.trim()  + '"' ;  
-        document.querySelector('#quote').innerHTML = data[1].content.rendered.trim();  
-        document.querySelector('#author').innerHTML = "-Author: " + data[1].title.rendered;    
+        document.querySelector('#quote').innerHTML = data[i].content.rendered.trim();  
+        document.querySelector('#author').innerHTML = "-Author: " + data[i].title.rendered;    
        
         
             
